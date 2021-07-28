bf9116d7e7a8284527e1d94537de1228c2b3abb8
Jonathan Sprinkle <sprinkjm@email.arizona.edu>
2021-07-28 10:22:04 -0700

    First checkin of reader from acc details

---
+++ setpointreader_data.cpp
@@ -0,0 +1,69 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: setpointreader_data.cpp
+//
+// Code generated for Simulink model 'setpointreader'.
+//
+// Model version                  : 1.45
+// Simulink Coder version         : 9.5 (R2021a) 14-Nov-2020
+// C/C++ source code generated on : Wed Jul 28 10:12:27 2021
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Generic->Unspecified (assume 32-bit Generic)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#include "setpointreader.h"
+#include "setpointreader_private.h"
+
+// Block parameters (default storage)
+P_setpointreader_T setpointreader_P = {
+  // Computed Parameter: Constant_Value
+  //  Referenced by: '<S1>/Constant'
+
+  {
+    {
+      0.0,                             // X
+      0.0,                             // Y
+      0.0                              // Z
+    },                                 // Linear
+
+    {
+      0.0,                             // X
+      0.0,                             // Y
+      0.0                              // Z
+    }                                  // Angular
+  },
+
+  // Computed Parameter: Out1_Y0
+  //  Referenced by: '<S4>/Out1'
+
+  {
+    0.0,                               // X
+    0.0,                               // Y
+    0.0                                // Z
+  },
+
+  // Computed Parameter: Constant_Value_j
+  //  Referenced by: '<S3>/Constant'
+
+  {
+    0.0,                               // X
+    0.0,                               // Y
+    0.0                                // Z
+  },
+
+  // Expression: 0.27777
+  //  Referenced by: '<Root>/Gain'
+
+  0.27777
+};
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
