50c7f167959a6fc80bca350e222ff742454bbe28
Jonathan Sprinkle <sprinkjm@email.arizona.edu>
2023-08-11 14:51:35 -0500

    Adding new versions

---
+++ include/setpointreader/setpointreader_private.h
@@ -0,0 +1,29 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: setpointreader_private.h
+//
+// Code generated for Simulink model 'setpointreader'.
+//
+// Model version                  : 4.0
+// Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+// C/C++ source code generated on : Fri Aug 11 14:36:26 2023
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Generic->Unspecified (assume 32-bit Generic)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef RTW_HEADER_setpointreader_private_h_
+#define RTW_HEADER_setpointreader_private_h_
+#include "rtwtypes.h"
+#include "setpointreader_types.h"
+#endif                                 // RTW_HEADER_setpointreader_private_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
---
+++ src/setpointreader_data.cpp
@@ -0,0 +1,68 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: setpointreader_data.cpp
+//
+// Code generated for Simulink model 'setpointreader'.
+//
+// Model version                  : 4.0
+// Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+// C/C++ source code generated on : Fri Aug 11 14:36:26 2023
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Generic->Unspecified (assume 32-bit Generic)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#include "setpointreader.h"
+
+// Block parameters (default storage)
+P_setpointreader_T setpointreader_P = {
+  // Computed Parameter: Constant_Value
+  //  Referenced by: '<S1>/Constant'
+
+  {
+    {
+      0.0,                             // X
+      0.0,                             // Y
+      0.0                              // Z
+    },                                 // Linear
+
+    {
+      0.0,                             // X
+      0.0,                             // Y
+      0.0                              // Z
+    }                                  // Angular
+  },
+
+  // Computed Parameter: Out1_Y0
+  //  Referenced by: '<S4>/Out1'
+
+  {
+    0.0,                               // X
+    0.0,                               // Y
+    0.0                                // Z
+  },
+
+  // Computed Parameter: Constant_Value_j
+  //  Referenced by: '<S3>/Constant'
+
+  {
+    0.0,                               // X
+    0.0,                               // Y
+    0.0                                // Z
+  },
+
+  // Expression: 1.0
+  //  Referenced by: '<Root>/Gain'
+
+  1.0
+};
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
